ac9f99d29ea1ea6552d48862a7134ed5fc7cbc40
AlaEddine122 <114233581+AlaEddine122@users.noreply.github.com>
2024-11-27 14:09:23 +0000

    Live scatter graph made, update to read from sheep positions array

--- src/sheep_simulation/sheep_simulation/visualizer.py
+++ src/sheep_simulation/sheep_simulation/visualizer.py
@@ -5,20 +5,33 @@ from matplotlib import style
 style.use('fivethirtyeight')
 
 fig = plt.figure()
+
 ax1 = fig.add_subplot(1,1,1)
 
 def animate(i):
-    graph_data = open('src/sheep_simulation/sheep_simulation/coords.txt','r').read()
+    #graph_data = sheep_positions
+    graph_data = open('coords.txt','r').read()
     lines = graph_data.split('\n')
     xs = []
     ys = []
+    '''for position in graph_data:
+            x,y = sheep_positions[(x,y)]
+            xs.append(float(x))
+            ys.append(float(y))
+        ax1.clear()
+        ax1.scatter(xs, ys)
+        plt.xlim(0,20)
+        plt.ylim(0,20)
+    '''
     for line in lines:
         if len(line) > 1:
             x, y = line.split(',')
             xs.append(float(x))
             ys.append(float(y))
     ax1.clear()
-    ax1.plot(xs, ys)
+    ax1.scatter(xs, ys)
+    plt.xlim(0,20)
+    plt.ylim(0,20)
 
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
