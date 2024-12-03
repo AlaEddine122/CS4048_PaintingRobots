d383422b488b1d4f6c863fa7de9ebf89aded237f
K-Jacob8 <k.jacob.22@abdn.ac.uk>
2024-12-03 19:08:12 +0000

    fix
    Co-authored-by: Alaeddine Boudjatit <AlaEddine122@users.noreply.github.com>

--- src/sheep_simulation/launch/launch.py
+++ src/sheep_simulation/launch/launch.py
@@ -1,15 +1,20 @@
 import launch
 import launch_ros.actions
+import os
+from ament_index_python.packages import get_package_share_directory
 
 
 # Launch simulation and rviz2 with preset configuration
 def generate_launch_description():
+    # Get the package share directory
+    package_share_dir = get_package_share_directory('sheep_simulation')
+    
     return launch.LaunchDescription([
         launch_ros.actions.Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            arguments=['-d', '/root/ros2_ws/CS4048_SheepHerding/src/sheep_simulation/config/sheep_simulation_config.rviz']
+            arguments=['-d', os.path.join(package_share_dir, 'sheep_simulation_config.rviz')]
         ),
         launch_ros.actions.Node(
             package='sheep_simulation',
